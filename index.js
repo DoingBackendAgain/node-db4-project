4203eae7300a421aba5f4e757aab0c7017d678d6
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2021-01-06 22:34:30 -0500

    boilder plate

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+//const rR = require("./data/routers/recipeRouter")
+//const iR = require("./data/routers/ingredientsRouter")
+
+const server = express()
+
+server.use(express.json())
+
+// server.use("/recipies", rR)
+// server.use("/ingredients", iR)
+
+const port = 9000;
+
+server.listen(port,()=>{
+    console.log(`Server is running at https://localhost${port}`)
+})
+
+module.exports = server
